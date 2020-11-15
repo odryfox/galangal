6edc43d5de69b9631d6f55a709ec7bda03f66ec2
Vyacheslav Rusov <odryfox@gmail.com>
2020-11-15 14:04:34 +0300

    Add tests for bot service

--- galangal/infrastructure/bot.py
+++ galangal/infrastructure/bot.py
@@ -20,6 +20,25 @@ class TelegramService(IBotService):
             parse_mode='Markdown',
         )
 
+    def _build_message_for_phrase_usages_in_different_languages(
+        self,
+        phrase_usages_in_different_languages: PhraseUsagesInDifferentLanguages,
+        languages: List[Language],
+    ) -> str:
+
+        message = ''
+
+        for phrase_usage_in_different_languages in phrase_usages_in_different_languages:
+            for language in languages:
+                phrase_usage = phrase_usage_in_different_languages[language]
+                parts = phrase_usage.text.split(phrase_usage.phrase)
+                target_text = "*{}*".format(phrase_usage.phrase).join(parts)
+                message += target_text + '\n'
+
+            message += '\n'
+
+        return message
+
     def send_phrase_usages_in_different_languages(
         self,
         chat_id: str,
@@ -27,19 +46,11 @@ class TelegramService(IBotService):
         languages: List[Language]
     ) -> None:
 
-        response = ''
-        for phrase_usage_in_different_languages in phrase_usages_in_different_languages:
-            for language in languages:
-                phrase_usage = phrase_usage_in_different_languages[language]
-                parts = phrase_usage.text.split(phrase_usage.phrase)
-                target_text = "*{}*".format(phrase_usage.phrase).join(parts)
-                response += target_text + '\n'
-
-            response += '\n'
-
-        updater = Updater(token=self._token)
-        updater.bot.send_message(
+        response = self._build_message_for_phrase_usages_in_different_languages(
+            phrase_usages_in_different_languages=phrase_usages_in_different_languages,
+            languages=languages,
+        )
+        self.send_message(
             chat_id=chat_id,
-            text=response,
-            parse_mode='Markdown',
+            message=response,
         )
---
+++ galangal/tests/infrastructure/test_bot.py
@@ -0,0 +1,120 @@
+from unittest import mock
+from unittest.mock import Mock
+
+from domain.constants import Language
+from domain.entities import PhraseUsage
+from infrastructure.bot import TelegramService
+
+
+class TestTelegramService:
+
+    def setup_method(self):
+        self.token = 'telegram_token'
+        self.service = TelegramService(token=self.token)
+
+    @mock.patch('infrastructure.bot.Updater')
+    def test_register_webhook(self, updater_mock):
+        url = 'https://server.com/webhook'
+
+        bot_mock = Mock()
+        updater_mock.return_value = Mock(bot=bot_mock)
+
+        self.service.register_webhook(url=url)
+
+        bot_mock.delete_webhook()
+        bot_mock.set_webhook.assert_called_once_with(url=url)
+
+    @mock.patch('infrastructure.bot.Updater')
+    def test_send_message(self, updater_mock):
+        chat_id = 'terminator_chat_id'
+        message = 'I will be back'
+
+        bot_mock = Mock()
+        updater_mock.return_value = Mock(bot=bot_mock)
+
+        self.service.send_message(
+            chat_id=chat_id,
+            message=message,
+        )
+
+        updater_mock.assert_called_once_with(token=self.token)
+        bot_mock.send_message.assert_called_once_with(
+            chat_id=chat_id,
+            text=message,
+            parse_mode='Markdown',
+        )
+
+    def test_build_message_for_phrase_usages_in_different_languages__empty_message(self):
+        message = self.service._build_message_for_phrase_usages_in_different_languages(
+            phrase_usages_in_different_languages=[],
+            languages=[],
+        )
+
+        assert message == ''
+
+    def test_build_message_for_phrase_usages_in_different_languages(self):
+        phrase_usages_in_different_languages = [
+            {
+                Language.EN: PhraseUsage(
+                    text="If anyone should phone, say I will be back at one o'clock.",
+                    phrase='I will be back',
+                ),
+                Language.RU: PhraseUsage(
+                    text='Если кто-нибудь позвонит, скажи, что я вернусь в час.',
+                    phrase='я вернусь',
+                ),
+            },
+            {
+                Language.EN: PhraseUsage(
+                    text='I will be back by 5, but just...',
+                    phrase='I will be back',
+                ),
+                Language.RU: PhraseUsage(
+                    text='Я вернусь к пяти, но если...',
+                    phrase='Я вернусь',
+                ),
+            },
+        ]
+
+        message = self.service._build_message_for_phrase_usages_in_different_languages(
+            phrase_usages_in_different_languages=phrase_usages_in_different_languages,
+            languages=[Language.EN, Language.RU],
+        )
+
+        assert message == """If anyone should phone, say *I will be back* at one o'clock.
+Если кто-нибудь позвонит, скажи, что *я вернусь* в час.
+
+*I will be back* by 5, but just...
+*Я вернусь* к пяти, но если...
+
+"""
+
+    @mock.patch.object(TelegramService, '_build_message_for_phrase_usages_in_different_languages')
+    @mock.patch.object(TelegramService, 'send_message')
+    def test_send_phrase_usages_in_different_languages(
+        self,
+        send_message_mock,
+        build_message_for_phrase_usages_in_different_languages_mock,
+    ):
+        chat_id = 'terminator_chat_id'
+        message = 'message'
+
+        build_message_for_phrase_usages_in_different_languages_mock.return_value = message
+
+        phrase_usages_in_different_languages = Mock()
+        languages = Mock()
+
+        self.service.send_phrase_usages_in_different_languages(
+            chat_id=chat_id,
+            phrase_usages_in_different_languages=phrase_usages_in_different_languages,
+            languages=languages,
+        )
+
+        build_message_for_phrase_usages_in_different_languages_mock.assert_called_once_with(
+            phrase_usages_in_different_languages=phrase_usages_in_different_languages,
+            languages=languages,
+        )
+        send_message_mock.assert_called_once_with(
+            chat_id=chat_id,
+            message=message,
+        )
