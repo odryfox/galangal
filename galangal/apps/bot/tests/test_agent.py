a04d3cf76bbfabe14f805fbe228f9191758cf8d3
Vyacheslav Rusov <odryfox@gmail.com>
2021-10-07 09:33:59 +0300

    Add test for agent

---
+++ galangal/apps/bot/tests/test_agent.py
@@ -0,0 +1,33 @@
+from bot.agent.agent import create_agent
+from bot.constants import ActionType
+from bot.markdown import Action
+from config import Config
+from language.services import RecognizeLanguageService
+from redis import Redis
+from search_phrase_contexts.daos import PhraseContextsDAO
+from search_phrase_contexts.use_cases import SearchPhraseContextsUseCase
+from vocabulary_trainer.daos import PhraseToStudyDAO
+from vocabulary_trainer.use_cases import (
+    AddPhraseToStudyUseCase,
+    SuggestPhrasesToStudyUseCase
+)
+
+
+def test_agent():
+    config = Config()
+    agent = create_agent(
+        add_phrase_to_study_use_case=AddPhraseToStudyUseCase(
+            phrase_to_study_dao=PhraseToStudyDAO(),
+        ),
+        search_phrase_contexts_use_case=SearchPhraseContextsUseCase(
+            recognize_language_service=RecognizeLanguageService(),
+            phrase_contexts_dao=PhraseContextsDAO(),
+        ),
+        suggest_phrases_to_study_use_case=SuggestPhrasesToStudyUseCase(),
+        redis=Redis.from_url(config.REDIS_URL),
+    )
+
+    action = Action(action_type=ActionType.GREETING, params={})
+    answers = agent.query(action, user_id='test')
+
+    assert answers == ['Привет, я бот, который поможет тебе выучить язык быстро']
