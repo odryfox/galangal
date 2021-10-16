4ef4f8d54692160c071a3860346a62eef2f291b6
Vyacheslav Rusov <odryfox@gmail.com>
2021-10-16 14:22:04 +0300

    Patch telegram services

--- galangal/apps/web/tests/conftest.py
+++ galangal/apps/web/tests/conftest.py
@@ -1,11 +1,28 @@
+from unittest import mock
+from unittest.mock import patch
+
 import pytest
 from config import Config
 from flask.testing import FlaskClient
 from web import create_app
 
 
-@pytest.fixture(scope='module', autouse=True)
-def client() -> FlaskClient:
+@pytest.fixture()
+def telegram_process_message_service_class():
+    telegram_process_message_service_class_mock = mock.Mock()
+    with patch('web.app.TelegramProcessMessageService', autospec=True):
+        yield telegram_process_message_service_class_mock
+
+
+@pytest.fixture()
+def telegram_register_webhook_service_class():
+    telegram_register_webhook_service_class_mock = mock.Mock()
+    with patch('web.app.TelegramRegisterWebhookService', autospec=True):
+        yield telegram_register_webhook_service_class_mock
+
+
+@pytest.fixture(autouse=True)
+def client(telegram_process_message_service_class, telegram_register_webhook_service_class) -> FlaskClient:
     config = Config()
     app = create_app(config)
 
