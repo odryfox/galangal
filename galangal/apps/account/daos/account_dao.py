3156a43ce98641baa1b77f6a935b045e357be0c4
Vyacheslav Rusov <odryfox@gmail.com>
2021-10-21 08:07:15 +0300

    Add processing duplicates accounts

--- galangal/apps/account/daos/account_dao.py
+++ galangal/apps/account/daos/account_dao.py
@@ -7,7 +7,16 @@ class AccountDAO:
     def create_account_by_chat_id(self, chat_id: str):
         session = Session()
 
+        instance = session.query(AccountModel).filter_by(
+            chat_id=chat_id,
+        ).one_or_none()
+        if instance:
+            return
+
         account_model = AccountModel(chat_id=chat_id)
 
-        session.add(account_model)
-        session.commit()
+        try:
+            session.add(account_model)
+            session.commit()
+        except Exception:
+            session.rollback()
--- galangal/apps/account/tests/daos/test_account_dao.py
+++ galangal/apps/account/tests/daos/test_account_dao.py
@@ -1,8 +1,19 @@
 from account.daos import AccountDAO
 from account.models import AccountModel
+from account.testing.factories import AccountModelFactory
 
 
 def test_create_account_by_chat_id(session):
+    dao = AccountDAO()
+    dao.create_account_by_chat_id(chat_id='example')
+
+    count = session.query(AccountModel).count()
+    assert count == 1
+
+
+def test_create_account_by_chat_id__already_exists(session):
+    AccountModelFactory(chat_id='example')
+
     dao = AccountDAO()
     dao.create_account_by_chat_id(chat_id='example')
 
